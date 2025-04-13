068ee5ab892a299824b22eaef993f58e8eeb40b7
xiangivyli <xiangivyli@gmail.com>
2025-04-13 20:51:16 +0100

    create a functional teamable app 1st version

--- post_website_template/javascript/app.js
+++ post_website_template/javascript/app.js
@@ -1,6 +1,10 @@
 function handleCreatePost() {
     document.getElementById("post-form-container").style.display = "block";
+    document.getElementById("author-info").style.display = "none";
+    document.getElementById("main-section-container").style.display = "none";
 }
-function hidePostForm() {
+function hidePostFormAndShowMain() {
     document.getElementById("post-form-container").style.display = "none";
+    document.getElementById("author-info").style.display = "block";
+    document.getElementById("main-section-container").style.display = "flex";
 }
