fc1cf4cf22c6178083f24c9d514fcf36e5709caa
xiangivyli <xiangivyli@gmail.com>
2025-06-22 10:00:50 +0100

    achieve sending frontend input to backend with middleware, json and req, res

--- nodejs-app/server.js
+++ nodejs-app/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json()); //resiter middleware to parse JSON bodies
 app.use('/', express.static(__dirname + '/'))
 
+app.post('/save-task', function(req, res) {
+    const taskObj = req.body
+    // next step is to connect to the database and save the task
+    console.log('Task received:', taskObj.task);
+    res.send({savedTask: taskObj.task});
+})
 
 app.listen(3000,function () {
     console.log('app listening on port 3000');
